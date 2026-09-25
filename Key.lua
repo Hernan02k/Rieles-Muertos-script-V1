642c4b4f3de6111e9d60b178cc3c4de19e772879
Hernan02k <hernan02k@gmail.com>
2026-09-25 04:41:02 -0500

    Create Key.lua

---
+++ Key.lua
@@ -0,0 +1,13 @@
+local Platoboost = loadstring(game:HttpGet("https://raw.githubusercontent.com/Platoboost/Platoboost-Roblox/main/Platoboost.lua"))()
+
+Platoboost:Init({
+    ServiceId = 32890,
+    HubName = "Multi-Tool Hub V11",
+    OnSuccess = function()
+        -- 1. Asigna tu Service ID oficial para pasar la verificación blindada
+        _G.MiServiceIDOficial = 32890
+        
+        -- 2. Descarga y ejecuta tu script principal desde la nueva URL limpia
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Hernan02k/Rieles-Muertos-script-V1/main/Rieles-V1.lua"))()
+    end
+})
