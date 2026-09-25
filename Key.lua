8bb985747862786dbf9dc9ed356730e0955e429b
Hernan02k <hernan02k@gmail.com>
2026-09-25 05:46:37 -0500

    Change Platoboost SDK URL and clean up code
    Updated the Platoboost SDK URL and removed HubName.

--- Key.lua
+++ Key.lua
@@ -1,13 +1,9 @@
-local Platoboost = loadstring(game:HttpGet("https://raw.githubusercontent.com/Platoboost/Platoboost-Roblox/main/Platoboost.lua"))()
+local Platoboost = loadstring(game:HttpGet("https://raw.githubusercontent.com/Platoboost/Platoboost-Roblox-SDK/main/SDK.lua"))()
 
 Platoboost:Init({
     ServiceId = 32890,
-    HubName = "Rieles-HubV1",
     OnSuccess = function()
-        -- 1. Asigna tu Service ID oficial para pasar la verificación blindada
         _G.MiServiceIDOficial = 32890
-        
-        -- 2. Descarga y ejecuta tu script principal desde la nueva URL limpia
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Hernan02k/Rieles-Muertos-script-V1/main/Rieles-V1.lua"))()
     end
 })
